a87eb4688ba9e917f2f6b700a08048ded0787751
An Pham <anpham148@gmail.com>
2020-08-10 13:37:12 +0000

    add popular menu class

--- lib/cocktail_maker.rb
+++ lib/cocktail_maker.rb
@@ -1,5 +1,7 @@
 require_relative "./cocktail_maker/version"
 require_relative "cocktail_maker/cli"
+require_relative "cocktail_maker/api"
+require_relative "cocktail_maker/popular_menu"
 
 require 'pry'
 module CocktailMaker
--- lib/cocktail_maker/cli.rb
+++ lib/cocktail_maker/cli.rb
@@ -1,6 +1,18 @@
 class CocktailMaker::CLI
   def call
-    puts "Welcome to Cocktail Maker!"
-    binding.pry
+    puts "Welcome to Cocktail Maker! How would you like to search for the cocktail recipe?"
+    get_user_input
+    # list_popular_menu
+    # get_recipes(name)
+    # get_recipes(ingredient)
+  end
+
+  def get_user_input
+     puts "1. I would like to have a look at the popular menu"
+     puts "2. I would like to search by name"
+     puts "3. I would like to search by ingredient"
+     puts "4. Surprise me"
+
+     input = gets.strip
   end
 end
