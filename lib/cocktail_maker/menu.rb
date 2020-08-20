052c192f0c1538a11a83ae8d1dc39e10c77060b7
An Pham <anpham148@gmail.com>
2020-08-20 14:44:37 +0000

    modify indentation

--- lib/cocktail_maker/menu.rb
+++ lib/cocktail_maker/menu.rb
@@ -1,39 +1,39 @@
 class CocktailMaker::Menu
   attr_accessor :menu
 
-   def initialize(menu)
-     @menu = menu
-   end
+  def initialize(menu)
+    @menu = menu
+  end
 
-   def display_menu
-     @menu.each.with_index(1) do |cocktail, index|
-      puts "#{index}. #{cocktail}".colorize(:light_magenta)
-     end
-   end
+  def display_menu
+    @menu.each.with_index(1) do |cocktail, index|
+    puts "#{index}. #{cocktail}".colorize(:light_magenta)
+    end
+  end
 
-   def get_cocktail_w_name
-      if @menu.count > 1
-        puts
-        get_your_cocktail(get_user_decision)
-      else
-        puts
-        get_your_cocktail(@menu.first)
-      end
-   end
+  def get_cocktail_w_name
+    if @menu.count > 1
+      puts
+      get_your_cocktail(get_user_decision)
+    else
+      puts
+      get_your_cocktail(@menu.first)
+    end
+  end
 
    def get_cocktail_by_pop_menu
+     result = CocktailMaker::Cocktail.find_or_create(get_user_decision)
+     new_menu = CocktailMaker::Menu.new(result)
+     new_menu.display_menu
 
-       result = CocktailMaker::Cocktail.find_or_create(get_user_decision)
-       new_menu = CocktailMaker::Menu.new(result)
-       new_menu.display_menu
-       if new_menu.menu.count > 1
-         a = new_menu.get_user_decision
-         puts
-         get_your_cocktail(a)
-       else
-         puts
-         get_your_cocktail(new_menu.menu.first)
-       end
+     if new_menu.menu.count > 1
+       a = new_menu.get_user_decision
+       puts
+       get_your_cocktail(a)
+     else
+       puts
+       get_your_cocktail(new_menu.menu.first)
+     end
    end
 
    def get_your_cocktail(name)
